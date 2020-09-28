29ff97b500d5d658b1a1163fabf5217f7d4b0bee
Sharon Yaroshetsky <imso3k@gmail.com>
2020-09-28 23:25:37 +0300

    0.5.0 - completed add_artist_album_info & sort_list_by_album funcs

--- organizer/organizer.py
+++ organizer/organizer.py
@@ -1,24 +1,29 @@
 import os
 import glob
+import operator
 from tinytag import TinyTag
-
-
-def move_to_folder():
-    pass
 
 
 def get_music_in_directory():
     return glob.glob('*.mp3') + glob.glob('*.m4a')
 
 
+def add_artist_album_info():
+    for i, file in enumerate(music_file_names):
+        tag = TinyTag.get(music_file_names[i])
+        music_file_names[i] = (tag.artist, tag.album, file)
+
+
 def sort_list_by_album():
+    music_file_names.sort(key=operator.itemgetter(1, 2))
+
+
+def move_to_folder():
     pass
 
 
 os.chdir('/home/sharon/Music/')
 music_file_names = get_music_in_directory()
-music_file_paths = [path for path in music_file_names]
-get_music_in_directory()
-print(music_file_paths)
-tag = TinyTag.get(music_file_names[0])
-print(tag.artist)
+add_artist_album_info()
+sort_list_by_album()
+print(music_file_names)
