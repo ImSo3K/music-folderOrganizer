41b2d9eaec54e1603224e7b846bc86ae3c397f84
Sharon Yaroshetsky <imso3k@gmail.com>
2020-09-28 23:00:05 +0300

    0.3.0 - added skeleton functions & got music from directory

--- organizer/organizer.py
+++ organizer/organizer.py
@@ -1,5 +1,24 @@
 import os
+import glob
 from tinytag import TinyTag
-tag = TinyTag.get('/home/sharon/Music/01 Irresistible.m4a')
+
+
+def move_to_folder():
+    pass
+
+
+def get_music_in_directory():
+    return glob.glob('*.mp3') + glob.glob('*.m4a')
+
+
+def sort_list_by_album():
+    pass
+
+
+os.chdir('/home/sharon/Music/')
+music_file_names = get_music_in_directory()
+music_file_paths = [path for path in music_file_names]
+get_music_in_directory()
+print(music_file_paths)
+tag = TinyTag.get(music_file_names[0])
 print(tag.artist)
-print(f'number of tracks: {tag.track_total}')
