18e5d7dec5930b6d1d3d5004cfc8771f8e8626fe
Sharon Yaroshetsky <imso3k@gmail.com>
2020-09-29 19:12:48 +0300

    0.6.5 - refactored the initialization of the sorted list algorithm/usr/bin/python3.8

--- organizer/organizer.py
+++ organizer/organizer.py
@@ -11,19 +11,39 @@ def get_music_in_directory(path):
 
 
 def add_artist_album_info():
-    for i, file in enumerate(music_file_names):
-        tag = TinyTag.get(music_file_names[i])
-        music_file_names[i] = (tag.artist, tag.album, file)
+    for i, file in enumerate(raw_file_names):
+        tag = TinyTag.get(raw_file_names[i])
+        raw_file_names[i] = (tag.artist, tag.album, file)
+    return
 
 
-def sort_list_by_album():
-    music_file_names.sort(key=operator.itemgetter(1, 2))
+def sort_raw_data():
+    raw_file_names.sort(key=operator.itemgetter(1, 2))
+    return
+
+
+def initialize_sorted_list():
+    album = []
+    while raw_file_names:
+        album_name = raw_file_names[0][1]
+
+        while album_name == raw_file_names[1][1]:
+            album.append(raw_file_names[0])
+            raw_file_names.pop(0)
+            if len(raw_file_names) == 1:
+                break
+
+        album.append(raw_file_names[0])
+        raw_file_names.pop(0)
+        sorted_music_info.append(album)
+        album = []
+    return
 
 
 def move_to_folder():
     i = 0
-    while i < len(music_file_names):
-        tag = TinyTag.get(music_file_names[i][2])
+    while i < len(raw_file_names):
+        tag = TinyTag.get(raw_file_names[i][2])
         folder_name = f'{tag.artist} - {tag.album} ({tag.year.split("-",1)[0]})'
         folder_name = re.sub('[!@#$/]', '', folder_name)
 
@@ -31,10 +51,10 @@ def move_to_folder():
             continue
         else:
             os.mkdir(folder_name)
-            while tag.album == music_file_names[i][1] and i < len(music_file_names):
-                shutil.move(music_file_names[i][2], folder_name)
+            while tag.album == raw_file_names[i][1] and i < len(raw_file_names):
+                shutil.move(raw_file_names[i][2], folder_name)
                 i += 1
-
+    return
 
 # testing purposes
 def extract_from_directories():
@@ -43,12 +63,16 @@ def extract_from_directories():
         files_to_extract = get_music_in_directory(directory)
         for file in files_to_extract:
             shutil.move(file, os.getcwd())
+        os.rmdir(directory)
 
 
 os.chdir('/home/sharon/Music/')
-music_file_names = get_music_in_directory('')
-#print(music_file_names)
-# add_artist_album_info()
-extract_from_directories()
-# sort_list_by_album()
+raw_file_names = get_music_in_directory('')
+sorted_music_info = []
+add_artist_album_info()
+sort_raw_data()
+d = sorted_music_info
+initialize_sorted_list()
+print(d)
 # move_to_folder()
+# extract_from_directories()
